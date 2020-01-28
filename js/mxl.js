5b896d9f4b3407bf8782bef0702ca9fb51e30a10
ahmadsans <ahmad@portcities.net>
2020-01-28 16:03:20 +0700

    [START] v2_developement

--- js/mxl.js
+++ js/mxl.js
@@ -5,13 +5,11 @@ var double_vocals = vocals.concat(vocals);
 var encrypted_status = localStorage.getItem("encrypted");
 
 function start_decrypt() {
-        var content_area = document.getElementsByClassName('entry-content')[0];
-        var paragraphs = content_area.querySelectorAll("p, h1");
-        for (var i = 0; i < paragraphs.length; i++) {
-            para_content = paragraphs[i].textContent;
-            content_encrypt = decrypt_content(para_content);
-            paragraphs[i].textContent=content_encrypt;
-        }
+        var content_area = document.getElementsByTagName("encrypted");
+        var encrypted_content = content_area[0].innerHTML
+        encrypt_parsed = encrypted_content.replace(/&amp;/g, '&')
+        var decrypted_content = decrypt_content(encrypt_parsed)
+        content_area[0].innerHTML = decrypted_content
 }
 
 function decrypt_content(contents) {
@@ -28,9 +26,12 @@ function decrypt_content(contents) {
             results += index_now;
         }
     }
-    return results;
+    result = results.replace(/&fskg;/g, "&nbsp;")
+    return result
 }
 
-jQuery(document).ready(function($){
-    start_decrypt()
-})
+function start_decryption() {
+    jQuery(document).ready(function($){
+        start_decrypt()
+    })
+}
