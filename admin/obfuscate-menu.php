07967ca1ffbce2d90c3da6b2b0a0185407aad8c6
ahmadsans <ahmad@portcities.net>
2020-01-29 12:53:32 +0700

    [IMP] improve admin menu

--- admin/obfuscate-menu.php
+++ admin/obfuscate-menu.php
@@ -17,20 +17,15 @@ function obfus_content() {
 <?php
     if ( isset($_POST['precheck']) && $_POST['precheck'] == "confirmed" ) {
         $new_ads1 = $_POST['ads1'];
-        $new_ads2 = $_POST['ads2'];
         update_option('obfuscate_ads1', $new_ads1);
-        update_option('obfuscate_ads2', $new_ads2);
         echo '<div class="updated notice"><p>Ads saved</p></div>';
     }
     $ads1 = stripslashes(get_option('obfuscate_ads1'));
-    $ads2 = stripslashes(get_option('obfuscate_ads2'));
 ?>
     <div><p>Ads that you want to put (optional)</p></div>
     <form name="obfus-options" method="post" action="">
         <div><p>Top and bottom ads:</p></div>
         <textarea name="ads1" rows="10" cols="60"><?php echo $ads1; ?></textarea><br>
-        <div><p>Center ads:</p></div>
-        <textarea name="ads2" rows="10" cols="60"><?php echo $ads2; ?></textarea><br>
         <input type="hidden" name="precheck" value="confirmed"><br>
         <button type="submit">Save</button>
     <?php
